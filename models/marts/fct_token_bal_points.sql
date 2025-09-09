cdb004981c9a623a9c3a45fcebb55551a8332789
Rodolfo Silva <rodolfo@serotonin.co>
2025-09-09 21:38:54 +0000

    fixed base multiplier template

--- models/marts/fct_token_bal_points.sql
+++ models/marts/fct_token_bal_points.sql
@@ -50,13 +50,13 @@ points_calculation AS (
       symbol,
       token_balance,
       holding_streak_days,
-      {{ base_multiplier('Solstice', symbol) }} AS base_mult,
-      token_balance * {{ base_multiplier('Solstice', symbol) }} AS base_points,
+      {{ base_multiplier("symbol") }} AS base_mult,
+      token_balance * {{ base_multiplier("symbol") }} AS base_points,
       {{ loyalty_multiplier('holding_streak_days') }} AS loyalty_mult,
-      {{ base_multiplier('Solstice', symbol) }} *  {{ loyalty_multiplier('holding_streak_days') }} AS overall_mult_nocap, --Add more multipliers
+      {{ base_multiplier("symbol") }} *  {{ loyalty_multiplier('holding_streak_days') }} AS overall_mult_nocap, --Add more multipliers
       CASE
-        WHEN {{ base_multiplier('Solstice', symbol) }} *  {{ loyalty_multiplier('holding_streak_days') }} > 10
-        THEN 10 ELSE {{ base_multiplier('Solstice', symbol) }} *  {{ loyalty_multiplier('holding_streak_days') }}
+        WHEN {{ base_multiplier("symbol") }} *  {{ loyalty_multiplier('holding_streak_days') }} > 10
+        THEN 10 ELSE {{ base_multiplier("symbol") }} *  {{ loyalty_multiplier('holding_streak_days') }}
     END AS overall_mult, --Add more multipliers
       {{ loyalty_tier_label('holding_streak_days') }} AS loyalty_label
 
