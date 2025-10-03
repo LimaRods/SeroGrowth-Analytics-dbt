d5d470d91513cc674d716cd0d410a59b83ddf815
Rodolfo Silva <rodolfo@serotonin.co>
2025-10-03 21:28:40 +0000

    updated usx_request for usx_message

--- models/staging/stg_yield_vault.sql
+++ models/staging/stg_yield_vault.sql
@@ -3,6 +3,7 @@ SELECT
     timestamp_ntz,
     signature,
     user,
+    user_shares,
     asset_mint AS token_mint_address,
     {{ token_symbol('asset_mint') }} AS symbol,
     {{ token_amount_adj('amount','asset_mint') }} AS amount, 
