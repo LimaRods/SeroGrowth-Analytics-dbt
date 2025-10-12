27e99dafa8b643bf84e63986c54957770f3b1062
Rodolfo Silva <rodolfo@serotonin.co>
2025-10-12 18:42:51 +0000

    mannualy fixed the tvl and mint

--- models/staging/stg_usx_mint_redeem.sql
+++ models/staging/stg_usx_mint_redeem.sql
@@ -1,12 +1,38 @@
-SELECT
-    id,
-    timestamp_ntz,
-    signature,
-    requestor AS user,
-    collateral_mint AS token_mint_address,
-    {{ token_symbol('collateral_mint') }} AS symbol,
-    {{ token_amount_adj('collateral_amount','collateral_mint') }} AS amount, 
-    type
-FROM
-    {{ source('internal', 'usx_mint_redeem') }}
 
+
+WITH base AS (
+    SELECT
+        id,
+        timestamp_ntz,
+        signature,
+        requestor AS user,
+        collateral_mint AS token_mint_address,
+        {{ token_symbol('collateral_mint') }} AS symbol,
+        {{ token_amount_adj('collateral_amount','collateral_mint') }} AS amount, 
+        type
+    FROM
+        {{ source('internal', 'usx_mint_redeem') }}
+),
+
+--  Manual fix for missing USDC collateral deposit (Sept 23, 2025)
+manual_fix AS (
+    SELECT
+        'manual_usdc_fix_20250923' AS id,
+        TO_TIMESTAMP_NTZ('2025-09-23 00:00:00') AS timestamp_ntz,
+        NULL AS signature,
+        NULL AS user,
+        'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v' AS token_mint_address,  -- USDC mint address
+        'USDC' AS symbol,
+        61522946.2823 AS amount,
+        'CONFIRM_MINT' AS type
+)
+
+--  Merge raw source data + manual correction
+SELECT
+*
+FROM (
+SELECT * FROM base
+UNION ALL
+SELECT * FROM manual_fix
+)
+ORDER BY timestamp_ntz
--- models/staging/stg_yield_vault.sql
+++ models/staging/stg_yield_vault.sql
@@ -1,14 +1,40 @@
+
+
+WITH base AS (
+    SELECT
+        id,
+        timestamp_ntz,
+        signature,
+        user,
+        user_shares,
+        asset_mint AS token_mint_address,
+        {{ token_symbol('asset_mint') }} AS symbol,
+        {{ token_amount_adj('amount','asset_mint') }} AS amount,
+        type
+    FROM
+        {{ source("internal", "yield_vault") }}
+),
+
+-- 🩵 Manual fix for missing USX lock (September 23, 2025)
+manual_fix AS (
+    SELECT
+        'manual_usx_lock_fix_20250923' AS id,
+        TO_TIMESTAMP_NTZ('2025-09-23 00:00:00') AS timestamp_ntz,
+        NULL AS signature,
+        NULL AS user,
+        NULL AS user_shares,
+        '6FrrzDk5mQARGc1TDYoyVnSyRdds1t4PbtohCD6p3tgG' AS token_mint_address,  -- USX mint address
+        'USX' AS symbol,
+        55728634.4898 AS amount,
+        'LOCK' AS type
+)
+
+--  Merge source + manual correction
 SELECT
-    id,
-    timestamp_ntz,
-    signature,
-    user,
-    user_shares,
-    asset_mint AS token_mint_address,
-    {{ token_symbol('asset_mint') }} AS symbol,
-    {{ token_amount_adj('amount','asset_mint') }} AS amount, 
-    type,
-FROM
-    {{ source("internal","yield_vault") }}
-
-
+    *
+FROM (
+    SELECT * FROM base
+    UNION ALL
+    SELECT * FROM manual_fix
+)
+ORDER BY timestamp_ntz
