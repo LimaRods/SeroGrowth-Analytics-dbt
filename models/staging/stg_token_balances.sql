1ef4d27134dcd719deb73d251519c2afd9c3d4f0
Rodolfo Silva <rodolfo@serotonin.co>
2026-02-04 20:22:11 +0000

    add new YT tokens

--- models/staging/stg_token_balances.sql
+++ models/staging/stg_token_balances.sql
@@ -9,10 +9,13 @@ SELECT
 
 FROM
     {{ source('internal','token_balances')}}
-WHERE  token IN ('6FrrzDk5mQARGc1TDYoyVnSyRdds1t4PbtohCD6p3tgG',
-                '3ThdFZQKM6kRyVGLG48kaPg5TRMhYMKY1iCRa9xop1WC',
-                'DDoYyEUcdkHV5a4NCPXDRL9f93NgPbqK9ZANAGL627wF',
+WHERE  token IN ('6FrrzDk5mQARGc1TDYoyVnSyRdds1t4PbtohCD6p3tgG', -- USX
+                '3ThdFZQKM6kRyVGLG48kaPg5TRMhYMKY1iCRa9xop1WC', -- eUSX
+                'DDoYyEUcdkHV5a4NCPXDRL9f93NgPbqK9ZANAGL627wF', -- YT-eUSX-11MAR26
                 --'Gz6LTebmfQqjbQD4C5NzqFN6PVWRd9pG3BJ4p4xHeDxF',
                 --'6K6bDA3f2heMYZQzbu3GDzx73zEXCeWZ58msfc1kDA6n',
-                'HQmMS5W34VcMtR85akhZgvypy7iqVWRXi282vwdf9eTX'
+                'HQmMS5W34VcMtR85akhZgvypy7iqVWRXi282vwdf9eTX', -- YT-USX-09FEB26
+                'Au8g11nXqXrUAmL14GM3gQnrnJnr4dcpgc5DNAnu9F9s', -- YT-USX-01JUN26
+                'GEYwnvNzqFXrLnNq4riXbn2ASnwU3cF8RXW6wXKHM4sw' -- YT-eUSX-01JUN26
+
                 ) -- USX, eUSX, YT AND ELP
